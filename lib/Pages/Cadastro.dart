d3a8911d2951373624aa1e9c277cfbc8b7470875
brunoqnodin <brunoqnodin@gmail.com>
2020-12-05 16:46:52 -0300

    Criação da Pagina de boas vindas

--- lib/Pages/Cadastro.dart
+++ lib/Pages/Cadastro.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Cadastro extends StatefulWidget {
@@ -8,6 +9,60 @@ class Cadastro extends StatefulWidget {
 class _CadastroState extends State<Cadastro> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      padding: EdgeInsets.only(top: 10),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image.asset("assets/home.png"),
+          Padding(
+            padding: EdgeInsets.only(top: 20, bottom: 10),
+            child: Text(
+              "Pronto para iniciar?",
+              style: TextStyle(
+                  color: Colors.grey,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+                "Agora precisamos sincronizar seus dados financeiros. Por favor, informe as contas bancárias do seu empreendimento.",
+              style: TextStyle(
+                color: Colors.black54,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(15),
+            child: Container(
+              height: 50,
+              width: double.infinity,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Color(0xFF22B995),
+              ),
+              child: SizedBox.expand(
+                child: FlatButton(
+                  onPressed: () {
+                        (){};
+                  },
+                  child: Text(
+                    "Sincronizar conta(s)",
+                    style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
